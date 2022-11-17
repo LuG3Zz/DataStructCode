f8fe19dd42ffa002bfd5daf0d5d5394610b3df35
LuG3Zz <LuG3Zz@Github.com>
2022-11-17 20:18:09 +0800

    add huffmantree

---
+++ 第5章/huffman.cpp
@@ -0,0 +1,98 @@
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+typedef struct HTNode {
+  int weight;
+  int parent, lchild, rchild;
+  char data;
+} HTNode, *HuffmanTree; // 定义结构体
+
+typedef char **HuffmanCode;
+
+void Select(HuffmanTree HT, int len, int &s1, int &s2) {
+  // s1最小，s2次小
+  int i, min1 = 0x3f3f3f3f, min2 = 0x3f3f3f3f;
+  for (i = 1; i <= len; i++) {
+    if (HT[i].weight < min1 && HT[i].parent == 0) {
+      min1 = HT[i].weight;
+      s1 = i;
+    } // 找s1
+  }
+  for (i = 1; i <= len; i++) {
+    if (HT[i].weight < min2 && HT[i].parent == 0 && i != s1) {
+      min2 = HT[i].weight;
+      s2 = i;
+    } // 找s2
+  }
+  cout << "s1:" << s1 << ";"
+       << "s2:" << s2 << ";" << endl;
+}; // 找两个最小权值结点的函数
+
+void CreateHuffmanTree(HuffmanTree &HT, int n) {
+  if (n <= 1) {
+    return;
+  }
+  int i, s1, s2, m; // m:所有结点数量
+  m = 2 * n - 1;
+  HT = new HTNode[m + 1];
+  for (i = 1; i < m; i++) {
+    HT[i].parent = 0;
+    HT[i].lchild = 0;
+    HT[i].rchild = 0;
+  }
+  cout << "请输入叶子结点的权值：" << endl;
+  for (i = 1; i <= n; i++) {
+    cin >> HT[i].weight;
+  }
+  for (i = n + 1; i <= m; i++) {
+    Select(HT, i - 1, s1, s2);
+    cout << "HT[s1]:" << HT[s1].weight << ";"
+         << "HT[s2]:" << HT[s2].weight << ";" << endl;
+    HT[s1].parent = i;
+    HT[s2].parent = i;
+    HT[i].lchild = s1;
+    HT[i].rchild = s2;
+    HT[i].weight = HT[s1].weight + HT[s2].weight;
+  }
+}
+void CreatHuffmanCode(HuffmanTree HT, HuffmanCode &HC, int n) {
+  int i, start, c, f;
+  HC = new char *[n + 1];
+  char *cd = new char[n];
+  cd[n - 1] = '\0';
+  for (i = 1; i <= n; ++i) {
+    start = n - 1;
+    c = i;
+    f = HT[i].parent;
+    while (f != 0) {
+      --start;
+      if (HT[f].lchild == c)
+        cd[start] = '0';
+      else
+        cd[start] = '1';
+      c = f;
+      f = HT[f].parent;
+    }
+    HC[i] = new char[n - start];
+    strcpy(HC[i], &cd[start]);
+  }
+  delete[] cd;
+}
+void show(HuffmanTree HT, HuffmanCode HC) {
+  for (int i = 1; i <= sizeof(HC) + 1; i++) {
+    cout << "权值为" << HT[i].weight << "的结点编码为：" << HC[i] << endl;
+  }
+}
+
+int main(int argc, char *argv[]) {
+  HuffmanTree HT;
+  HuffmanCode HC;
+  int n;
+  cout << "请输入叶子结点的个数" << endl;
+  cin >> n;
+  CreateHuffmanTree(HT, n);
+  CreatHuffmanCode(HT, HC, n);
+  show(HT, HC);
+  return 0;
+}
